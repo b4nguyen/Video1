86503ca4ab41662ab475dab4f9c82612bbaa1a52
b4nguyen <97714611+b4nguyen@users.noreply.github.com>
2022-02-04 02:12:29 -0800

    1

--- UwU.java
+++ UwU.java
@@ -1,1 +1,8 @@
+public class UwU 
+{
+	public static int add(int u, int w)
+	{
+	return u + w + u;
+	}
+}
 
---
+++ UwUTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class UwUTest
+{
+	@Test
+	public void addition()
+	{
+		assertEquals(2, UwU.add(1,1));
+	}
+}
